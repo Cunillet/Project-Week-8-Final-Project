6c7f9940cbd516a7e49e766a3c52a7d36e5e02b0
Oriol Cunill <cunillati@gmail.com>
2020-03-06 16:25:29 +0100

    scrapy basic implementation

---
+++ amazon_recomender/amazon/launcher.py
@@ -0,0 +1,10 @@
+from scrapy.crawler import CrawlerProcess
+from amazon.spiders.AmazonProductSpider import AmazonProductSpider
+
+process = CrawlerProcess(settings={
+    'FEED_FORMAT': 'json',
+    'FEED_URI': 'items.json'
+})
+
+process.crawl(AmazonProductSpider)
+process.start() 
